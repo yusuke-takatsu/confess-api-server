337c42c880d894874223bb18d58221fec3da168b
YusukeTakatsu <github ys.taka44@gmail.com>
2024-09-07 18:26:23 +0900

    ユーザー登録処理作成

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\LoginRequest;
+use App\Http\Requests\User\RegisterRequest;
+use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,6 +24,26 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'ログインしました。'
+        ]);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $existsEmail = User::where('email', $request->email)->exists();
+
+        if ($existsEmail) {
+            return response()->json([
+                'message' => 'メールアドレスがすでに登録されています。'
+            ]);
+        }
+
+        User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'ユーザ登録が完了しました。',
         ]);
     }
 }
