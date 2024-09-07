f48fe59662b03158ac42d112b519df1f0f93c167
YusukeTakatsu <github ys.taka44@gmail.com>
2024-09-07 20:30:05 +0900

    リレーションメソッド作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
