4848143abbbeee674616860cf8373b837f398ed3
YusukeTakatsu <github ys.taka44@gmail.com>
2024-09-07 12:49:59 +0900

    ログイン処理追加

---
+++ app/Http/Controllers/User/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\Auth\LoginRequest;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!Auth::guard('user')->attempt($credentials)) {
+          throw new AuthenticationException('ログインに失敗しました。');
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'message' => 'ログインしました。'
+        ]);
+    }
+}
