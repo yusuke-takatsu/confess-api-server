570dae4f6897ec8a82189f63213ab736ce0ca24f
yusuke-takatsu <87213148+yusuke-takatsu@users.noreply.github.com>
2024-10-02 18:29:05 +0900

    Revert "ログイン処理追加"

--- config/auth.php
+++ config/auth.php
@@ -40,10 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'user' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ]
     ],
 
     /*
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -19,9 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password'),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
     }
 
