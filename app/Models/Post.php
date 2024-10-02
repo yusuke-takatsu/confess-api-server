5a52f493939e39cf7a9958117945789eab8ad240
YusukeTakatsu <github ys.taka44@gmail.com>
2024-10-02 20:46:34 +0900

    model修正

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,8 +15,13 @@ class Post extends Model
       'updated_at',
   ];
 
-  public function category()
+  public function user()
   {
       return $this->belongsTo(User::class);
+  }
+
+  public function category()
+  {
+      return $this->belongsTo(Category::class);
   }
 }
