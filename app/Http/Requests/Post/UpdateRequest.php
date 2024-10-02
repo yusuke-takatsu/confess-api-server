d63a8f5005a60afc4bbb7b4b7b9f13c1078eb2cc
YusukeTakatsu <github ys.taka44@gmail.com>
2024-10-02 21:23:17 +0900

    懺悔更新処理作成

---
+++ app/Http/Requests/Post/UpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => ['required', 'integer'],
+            'category_id' => ['required', 'integer', 'exists:categories,id'],
+            'content' => ['required', 'string']
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'category_id' => 'カテゴリ',
+            'content' => '内容',
+        ];
+    }
+}
